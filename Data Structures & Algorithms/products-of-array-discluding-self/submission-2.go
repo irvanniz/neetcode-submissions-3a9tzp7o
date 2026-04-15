8ef9d4ff28db679898aafe14ac7cf1a1644ca9e5
Van Jared <niero.neo@gmail.com>
2026-04-15 07:09:08 +0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.go
@@ -0,0 +1,35 @@
+func productExceptSelf(nums []int) []int {
+	totalNums := len(nums)
+	var (
+		prefix []int
+		p int = 1
+	)
+	for i:=0; i<totalNums; i++ {
+		if i == 0 {
+			prefix = append(prefix, 1)	
+			continue
+		}
+		p*=nums[i-1]
+		prefix = append(prefix, p)
+	}
+
+	var (
+		suffix = make([]int, totalNums)
+		s int = 1
+	)
+	
+	for i:=(totalNums-1); i>=0; i-- {
+		if i == 0 {
+			suffix[totalNums-1] = 1
+			continue
+		}
+		s*=nums[i]
+		suffix[i-1] = s
+	}
+
+	var result []int
+	for i, v := range prefix {
+		result = append(result, v*suffix[i])
+	}
+	return result
+}
