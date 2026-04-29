d64a4f2e9a10bf0af67bcc8ed5ff9b10a68b2b19
Van Jared <niero.neo@gmail.com>
2026-04-29 07:03:34 +0700

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.go
@@ -0,0 +1,31 @@
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	numsMap := make(map[int]bool)
+
+	for _, v := range nums {
+		numsMap[v] = true
+	}
+
+	maxLength := 1
+	for _, v := range nums {
+		if numsMap[v-1] {
+			continue
+		}
+
+		tempMaxLength := 1
+		i := v+1
+		for numsMap[i]{
+			i++
+			tempMaxLength++
+		}
+
+		if tempMaxLength > maxLength {
+			maxLength = tempMaxLength
+		}
+	}
+
+	return maxLength
+}
